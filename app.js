59d84cc118a68c8bb6ee636e4456d40685f1961b
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-08-22 13:27:05 +0200

    added MVP structure and fortune fuction

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 // MODEL
+const answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don not count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+];
 
 
 
 // VIEW
-
-
-
+updateView();
+function updateView(){
+    document.getElementById('app').innerHTML = /*HTML*/ `
+    <div id="app">Answer: </div>
+    <br/>
+    <button onclick="fortune()">Get fortune</button>
+    `;
+}
 // CONTROLLER
+/*function getFortune(answers) {
+    return answers[ Math.floor(Math.random() * answers.length)];
+    updateView();
+}
+*/
+
+function fortune() {
+    let randomEl = answers[Math.floor(Math.random() * answers.length)];
+    document.getElementById('app').textContent = `Answer: ${randomEl}`;
+}
+
+ 
+   
